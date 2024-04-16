b1f715e64bb33715ea84ba00d76e85f60a04ea6e
kimminam <bluemind8520@gmail.com>
2024-04-16 23:31:28 +0900

    Connect DataBase

--- proscons/pages/api/insert_pros_cons.js
+++ proscons/pages/api/insert_pros_cons.js
@@ -1,27 +1,27 @@
-const connection = require('./route');
+const { connection } = require('./route')
 
 function queryPromise(queryString, values) {
   return new Promise((resolve, reject) => {
-    db.query(queryString, values, (error, results) => {
+    connection.query(queryString, values, (error, results) => {
       if (error) {
-        return reject(error);
+        return reject(error)
       }
-      resolve(results);
-    });
-  });
+      resolve(results)
+    })
+  })
 }
 
 export default async function handler(req, res) {
-  const {} = req.body;
+  const { keyword_name, keyword_type, keyword_text } = req.body
 
-  let queryString = `INSERT INTO keyword_table (keyword_name,keyword_type,keyword_text) VALUES (?, ?, ?)`;
-  let values = [student_name, student_id, reservation, start_time, end_time];
+  let queryString = `INSERT INTO keyword_table (keyword_name,keyword_type,keyword_text) VALUES (?, ?, ?)`
+  let values = [keyword_name, keyword_type, keyword_text]
 
   try {
-    const rows = await queryPromise(queryString, values);
-    res.status(200).json(rows);
+    const rows = await queryPromise(queryString, values)
+    res.status(200).json(rows)
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
+    console.error(error)
+    res.status(500).send('Internal Server Error')
   }
 }
