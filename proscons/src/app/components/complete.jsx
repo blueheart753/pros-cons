35de64d05d67e9d60c688ec26c88d63e5bdd3e96
kimminam <bluemind8520@gmail.com>
2024-04-20 01:31:14 +0900

    finish complete modal css - fix duration 0.075s -> 0.2s - delete position absolute - delete check_box animation
    add close button and style

--- proscons/src/app/components/complete.jsx
+++ proscons/src/app/components/complete.jsx
@@ -1,38 +1,36 @@
-import Image from 'next/image'
 import { useEffect } from 'react'
+import Image from 'next/image'
 
 const CompleteModal = () => {
   useEffect(() => {
     const check_box = document.querySelector('#check_box')
     const check_image = document.querySelector('#checkImg')
-
+    const closeBtn = document.querySelector('#closeBtn')
     setTimeout(() => {
       if (check_box) {
-        check_box.classList.add(
-          'border-solid',
-          'border-4',
-          'border-black',
-          'animate-spin'
-        )
+        check_box.classList.replace('border-y-4', 'border-4')
+        check_box.classList.replace('opacity-0', 'opacity-1')
+      }
+      if (check_image) {
         check_image.classList.replace('opacity-0', 'opacity-1')
       }
-    }, 1000)
-
-    setTimeout(() => {
-      if (check_box) {
-        check_box.classList.remove('animate-spin')
+      if (closeBtn) {
+        closeBtn.classList.replace('opacity-0', 'opacity-1')
       }
-    }, 2000)
+    }, 500)
   }, [])
 
   return (
-    <div className="w-full h-full flex flex-col items-center absolute top-0 bottom-0 left-0 right-0 p-4 gap-36">
+    <div className="w-full h-full flex flex-col items-center p-4 gap-36">
       <div>
         <p className="text-black text-2xl">
           <span className="font-semibold">장점</span> 작성이 완료됬습니다.
         </p>
       </div>
-      <div className="p-4 rounded-full duration-75" id="check_box">
+      <div
+        className="p-4 rounded-full ease-in duration-200 border-solid border-y-4 border-black opacity-0"
+        id="check_box"
+      >
         <Image
           src={'/check-mark-svgrepo-com.svg'}
           alt="upload complete image"
@@ -42,6 +40,14 @@ const CompleteModal = () => {
           className="opacity-0"
         />
       </div>
+      <button
+        id="closeBtn"
+        type="button"
+        className="w-3/6 mt-4 bg-green-500 text-white p-2 rounded-md opacity-0"
+        onClick={() => (location.href = '/')}
+      >
+        닫기
+      </button>
     </div>
   )
 }
